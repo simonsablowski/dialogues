acfd4b9fe1ba4cbdc032c6d51708f7a7d5628e4a
Simon Sablowski <contact@simsab.net>
2011-08-11 21:27:46 +0000

    modified configuration for live setup

--- configuration.php
+++ configuration.php
@@ -4,18 +4,18 @@ $configuration = array();
 
 $configuration['pathApplication'] = dirname(__FILE__) . '/';
 
-$configuration['basePath'] = 'http://localhost/dialogue/';
+$configuration['basePath'] = '/';
 
 $configuration['includeDirectories'] = array(
 	$configuration['pathApplication'],
-	'D:/Webprojekte/cheese/',
-	'D:/Webprojekte/nacho/'
+	$configuration['pathApplication'] . '../cheese/',
+	$configuration['pathApplication'] . '../nacho/'
 );
 
 $configuration['Database'] = array(
 	'type' => 'MySql',
 	'host' => 'localhost',
-	'name' => 'dialogue',
+	'name' => 'dialogues',
 	'user' => 'root',
 	'password' => ''
 );
@@ -27,5 +27,4 @@ $configuration['Request'] = array(
 	)
 );
 
-$configuration['debugMode'] = TRUE;
-// $configuration['debugMode'] = FALSE;
+$configuration['debugMode'] = FALSE;
